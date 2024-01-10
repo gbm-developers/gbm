b1d6e97855489e2c4be3fa4ebebf8aa0cd7f19cf
Greg Ridgeway <gridge@sas.upenn.edu>
2024-01-10 17:21:37 -0500

    Ass startup message noting gbm3 is in active development

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 #' @keywords internal
 .onAttach <- function(lib, pkg) {
   vers <- utils::packageVersion("gbm")
-  packageStartupMessage(paste("Loaded gbm", vers))
+  packageStartupMessage("Loaded gbm ", vers)
+  packageStartupMessage("This version of gbm is no longer under development. Consider transitioning to gbm3, https://github.com/gbm-developers/gbm3")
 }
